83eb49c09b3cca80f6cad1c4a1a3c375764218b2
BruceChen <sharkmansky@163.com>
2022-10-12 17:07:14 +0800

    实现了评论详情界面

--- pages/group/replyToMe/replyToOthers/replyToOthers.js
+++ pages/group/replyToMe/replyToOthers/replyToOthers.js
@@ -5,6 +5,55 @@ Page({
    * 页面的初始数据
    */
   data: {
+    userName : '丁真珍珠',
+    postTime : '2022/10/10 15:15:15',
+    postText : '武大的同学们你们好，我是丁真珍珠。疫情期间，请大家出示自己的健康码，让我测一测你们的码。谢谢同学们的配合，我随后会为大家送上我最新代言的锐克五代电子烟。',
+    commentData : [
+      {
+        replyerName : '武大王源',
+        replyerAvartar : '/images/WHU_logo.png',
+        replyTime : '2022/10/10 15:15:15',
+        replyContent : '感觉不如传统香烟。。。好抽'
+      },
+      {
+        replyerName : '武大柯洁',
+        replyerAvartar : '/images/WHU_logo.png',
+        replyTime : '2022/10/10 15:15:15',
+        replyContent : '弈！悟！'
+      },
+      {
+        replyerName : '武大otto',
+        replyerAvartar : '/images/WHU_logo.png',
+        replyTime : '2022/10/10 15:15:15',
+        replyContent : '感觉不如口味王'
+      },
+      {
+        replyerName : '武大孙笑川',
+        replyerAvartar : '/images/WHU_logo.png',
+        replyTime : '2022/10/10 15:15:15',
+        replyContent : '凭什么测我的码？测测你的！'
+      },
+      {
+        replyerName : '武大若',
+        replyerAvartar : '/images/WHU_logo.png',
+        replyTime : '2022/10/10 15:15:15',
+        replyContent : '卧槽，bin！'
+      },
+      {
+        replyerName : 'csf',
+        replyerAvartar : '/images/WHU_logo.png',
+        replyTime : '2022/10/10 15:15:15',
+        replyContent : '1！5！'
+      },
+      {
+        replyerName : 'lzx',
+        replyerAvartar : '/images/WHU_logo.png',
+        replyTime : '2022/10/10 15:15:15',
+        replyContent : '?'
+      }
+    ],
+
+    commentContent : '',
 
   },
 
@@ -62,5 +111,39 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  /**
+   * 捕获评论输入值 
+   */
+  captureComment(e){
+    let content = e.detail.value;
+    console.log("要发布的评论是"+content);
+    this.setData({
+      commentContent : content
+    })
+  },
+
+  /**
+   * 发布评论 
+   */
+  postComment(e){
+    console.log("发布评论"+this.data.commentContent);
+    //TODO:将评论内容上传到数据库
+
+
+
+    //弹窗确认并且清零输入框
+    wx.showModal({
+      content: '评论已发布',
+      title: '提示',
+      showCancel : false,
+      success: (result) => {},
+      fail: (res) => {},
+      complete: (res) => {},
+    })
+    this.setData({
+      commentContent : ''
+    })
   }
 })
