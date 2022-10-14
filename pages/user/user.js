bcfdd789166f8c5151b4d9bf7cdd913a287c6280
luzhixing12345 <luzhixing12345@163.com>
2022-10-14 17:54:53 +0800

    [perf] : user page performance

--- pages/user/user.js
+++ pages/user/user.js
@@ -9,17 +9,10 @@ Page({
   onShow() {
     this.setData({
       userInfo: app.globalData.userInfo,
-      avatarUrl : app.globalData.userInfo.avatarUrl
    })
   },
   onLoad() {
   },
-
-  reportErr() {
-    wx.navigateTo({
-      url: './reportErr/reportErr',
-    })
-  }
 
     
 })
