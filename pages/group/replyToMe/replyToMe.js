3b16ff8fce03c1f72527167d6c9e243938703ac5
BruceChen <sharkmansky@163.com>
2022-10-25 18:24:23 +0800

    回复功能以及页面基本完成

--- pages/group/replyToMe/replyToMe.js
+++ pages/group/replyToMe/replyToMe.js
@@ -1,4 +1,7 @@
 // pages/group/replyToMe/replyToMe.js
+const app = getApp();
+var timeUtil = require("../../utils/time");
+
 Page({
 
   /**
@@ -6,92 +9,46 @@ Page({
    */
   data: {
     //三项数据只用作测试，更新后可集成到一个对象中进行遍历。
-    textArray:[
-      '满身烟味的我 走路带上点浮夸',
-      '想买悦刻五代找我丁真就对了',
-      '竖起中指王源他算个寄吧',
-      '台上打饱嗝我在和雪豹比划',
-      '丁真势力正在不断地扩大',
-      '微博之夜掏裤裆我上上下下',
-      '珈乐心理放线被我轻松击垮',
-      '甜美微笑爆杀那些小题做题家',
-      '脚叫做勾八 八八八八八',
-      '对你说藏话 啦啦啦啦啦',
-      '你不会回笼 回家找你妈',
-      '抽传统香烟 我测你们码',
-      '偶尔骑骑小马 理塘走一走',
-      '随手掏出悦刻 你也来一口',
-      '哥们不费力 就住进了高楼',
-      '理塘高速路 都 五档起步走',
-      '去联合国学英文 哥们Let\'s go',
-      '回理塘实践 哥们 Let\'s 抽',
-      '哥们哇啦哇啦乱唱听感就拉满了',
-      '你文化程度再高你也听不懂的',
-      '感觉这首歌技术不如丁真',
-      '因为你是肥猪 体重要按吨',
-      '我投资悦刻 别他妈倒闭了',
-      '有烟弹寄一个 给礼堂的丁真',
-      '我特么想抽烟(快给我抽)',
-      '抽死我个byd(快给我抽)',
-      '抽 抽 抽 抽 抽抽抽(快给我抽)',
-      '(快给我抽)',
-      '(快给我抽)',
-      '(快给我抽)',
-      '快给我 快给我 快给我 快给我 我要抽 我要抽 我要抽 我要抽',
-      'I GOT 油 我想抽',
-      'I GOT 油 ALL MY MIND',
-      '抽不了兜着走',
-      '走走走走走走走',
-      'I GOT 油 我想抽',
-      'I GOT 油 ALL MY MIND',
-      '抽不了兜着走',
-      '走走走走走走走',
-      '那些说唱 都是一坨屎',
-      '攻击性没有 词汇还低能的要死',
-      '但我心怀善念 接纳悦刻五的每个孝子',
-      '我的回笼技术能够气死那些黑子',
-      '一眼丁真 鉴定为纯纯的若智',
-      '牙医丁真 鉴定为纯纯的白齿',
-      '经过A门的时候帮我把烟雾给封死',
-      '王源不发龙狙证明他并没有素质',
-      'I GOT 油 我想抽',
-      'I GOT 油 ALL MY MIND',
-      '抽不了兜着走',
-      '走走走走走走走',
-      'I GOT 油 我想抽',
-      'I GOT 油 ALL MY MIND',
-      '抽不了兜着走',
-      '走走走走走走走',
-      '打完狙我准备骑珍珠去抽一根 哦',
-      '但是珍珠不在了',
-      '我只能骑着我的纠纠',
-      '穿越整个四川找悦刻旗舰店',
-      '为了芋泥啵啵 我抽胖了双脸',
-      '但是人们依然爱我 爱我纯真双眼',
-      '为了苦练烟嗓我抽烟好几年',
-      '现在一唱歌哥们嗓子就漏电',
-      '我爱抽烟 出生到太平间',
-      '我爱抽烟 一天十根直到肺病变',
-      '不要抽 假的烟 不要抽',
-      '如果你不知道去哪里就来我直播间',
-      '为了买烟我付出太多',
-      '没有电子烟的人生就只是片荒漠',
-      '没有悦刻的人生我也只是个喽啰',
-      '看到不认识的烟杆我会去抚摸',
-      '这一段我也不知道我在唱什么',
-      '也许我也只是溜大了',
-      '也许我只不过是溜大了',
-      '就像你的人生糊弄糊弄不就完了么'],
-      replyerName:'锐克首席代言人丁真',
-      replyTime:'2022/10/10 15:15:15'
+      userName : app.globalData.userInfo.nickName,
     },
   
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(options) {
+    var that = this;
+    const db = wx.cloud.database();
+    db.collection('comment').where({
+      _openid : app.globalData.userInfo._openid,
+    }).get({
+      success:(res)=>{
+        var tempList = [];
+        var dataList = res.data;
+        for(var i = 0; i < dataList.length; ++i){
+          var tempcfc = dataList[i].cfc;
+          var oriCmt = dataList[i].content;
+          var oriCmtId = dataList[i]._id;
+          for(var j = 0; j < tempcfc.length; ++j){
+            var temp = {};
+            temp.replyContent = tempcfc[j].content;
+            temp.replyTime = timeUtil.displayRelativeTime(tempcfc[j].time);
+            temp.replyerName = tempcfc[j].userInfo.nickName;
+            temp.replyerAvatarUrl = tempcfc[j].userInfo.avatarUrl;
+            temp.originComment = (oriCmt.length>10)?(oriCmt.substring(0,10)+'...'):(oriCmt);
+            temp.originCommentId = oriCmtId;
+            tempList.push(temp);
+          }  
+        }
+        that.setData({
+          replyList : tempList,
+        })
+        
+      },
+      fail:(res)=>{
+        console.log('查询失败');
+      },
+    })
   },
 
   /**
@@ -147,6 +104,11 @@ Page({
   reply(e){
     wx.navigateTo({
       url: '../replyToMe/replyToOthers/replyToOthers',
+      success:(res)=>{
+        res.eventChannel.emit('acceptDataFromReplyToMe',{
+          commentId : e.currentTarget.dataset.commentid,
+        })
+      }
     })
   }
 })
--- pages/group/replyToMe/replyToOthers/replyToOthers.js
+++ pages/group/replyToMe/replyToOthers/replyToOthers.js
@@ -1,65 +1,69 @@
 // pages/group/replyToMe/replyToOthers/replyToOthers.js
+var timeUtil = require('../../../utils/time')
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
-    userName : '丁真珍珠',
-    postTime : '2022/10/10 15:15:15',
-    postText : '武大的同学们你们好，我是丁真珍珠。疫情期间，请大家出示自己的健康码，让我测一测你们的码。谢谢同学们的配合，我随后会为大家送上我最新代言的锐克五代电子烟。',
-    commentData : [
-      {
-        replyerName : '武大王源',
-        replyerAvartar : '/images/WHU_logo.png',
-        replyTime : '2022/10/10 15:15:15',
-        replyContent : '感觉不如传统香烟。。。好抽'
-      },
-      {
-        replyerName : '武大柯洁',
-        replyerAvartar : '/images/WHU_logo.png',
-        replyTime : '2022/10/10 15:15:15',
-        replyContent : '弈！悟！'
-      },
-      {
-        replyerName : '武大otto',
-        replyerAvartar : '/images/WHU_logo.png',
-        replyTime : '2022/10/10 15:15:15',
-        replyContent : '感觉不如口味王'
-      },
-      {
-        replyerName : '武大孙笑川',
-        replyerAvartar : '/images/WHU_logo.png',
-        replyTime : '2022/10/10 15:15:15',
-        replyContent : '凭什么测我的码？测测你的！'
-      },
-      {
-        replyerName : '武大若',
-        replyerAvartar : '/images/WHU_logo.png',
-        replyTime : '2022/10/10 15:15:15',
-        replyContent : '卧槽，bin！'
-      },
-      {
-        replyerName : 'csf',
-        replyerAvartar : '/images/WHU_logo.png',
-        replyTime : '2022/10/10 15:15:15',
-        replyContent : '1！5！'
-      },
-      {
-        replyerName : 'lzx',
-        replyerAvartar : '/images/WHU_logo.png',
-        replyTime : '2022/10/10 15:15:15',
-        replyContent : '?'
-      }
-    ],
-
-  },
+  data: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  updateComment(){
+    const db = wx.cloud.database();
+    var that = this;
+    db.collection('comment').doc(this.data.commentId).get({
+      success:(res)=>{
+        var cmtctt = res.data.content;
+        var cmtusravt = res.data.userInfo.avatarUrl;
+        var cmtusrnam = res.data.userInfo.nickName;
+        var cmttim = timeUtil.displayRelativeTime(res.data.time);
+        var cmtlike = res.data.like;
+        var cmtdislike = res.data.dislike;
+        var cmtcfc = res.data.cfc;
+        var tempList = [];
+        for(var i = 0; i < cmtcfc.length; ++i){
+          var temp = {};
+          temp.avatarUrl = cmtcfc[i].userInfo.avatarUrl;
+          temp.nickName = cmtcfc[i].userInfo.nickName;
+          temp.time = timeUtil.displayRelativeTime(cmtcfc[i].time);
+          temp.content = cmtcfc[i].content;
+          temp.like = cmtcfc[i].like;
+          temp.dislike = cmtcfc[i].dislike;
+          temp.index = i;
+          tempList.push(temp);
+        }
+        
+        that.setData({
+          commentContent : cmtctt,
+          commentUserAvatarUrl : cmtusravt,
+          commentUserName : cmtusrnam,
+          commentTime : cmttim,
+          commentLike : cmtlike,
+          commentDislike : cmtdislike,
+          commentCfcList : tempList,
+        })
+      },
+      fail:(res)=>{
+        console.log('查询失败');
+        console.log(res);
+      }, 
+    })
+  },
+  onLoad:function(options){
+    var that = this;
+    const eC = this.getOpenerEventChannel();
+    eC.on('acceptDataFromReplyToMe',function(data){
+      that.setData({
+        commentId : data.commentId,
+      });
+    })
+    this.updateComment();
+    
+    
   },
 
   /**
@@ -118,7 +122,7 @@ Page({
     let content = e.detail.value;
     console.log("要发布的评论是"+content);
     this.setData({
-      commentContent : content
+      yourCommentContent : content
     })
   },
 
@@ -126,22 +130,115 @@ Page({
    * 发布评论 
    */
   postComment(e){
-    console.log("发布评论"+this.data.commentContent);
+    console.log("发布评论"+this.data.yourCommentContent);
     //TODO:将评论内容上传到数据库
+    const db = wx.cloud.database();
+    const _ = db.command;
+    
+    var temp = {};
+    temp.content = this.data.yourCommentContent;
+    temp.dislike = 0;
+    temp.like = 0;
+    temp.time = new Date();
+    temp.userInfo = app.globalData.userInfo;
 
-
-
-    //弹窗确认并且清零输入框
-    wx.showModal({
-      content: '评论发布成功',
-      title: '提示',
-      showCancel : false,
-      success: (result) => {},
-      fail: (res) => {},
-      complete: (res) => {},
+    var that = this;
+    db.collection('comment').doc(this.data.commentId).update({
+      data : {
+        cfc : _.push(temp)
+      },
+      success:(res)=>{
+        console.log('查询成功');
+        that.updateComment();
+      },
+      fail:()=>{
+        console.log('查询失败');
+      }
     })
     this.setData({
-      commentContent : ''
+      yourCommentContent : ''
+    })
+  },
+  cmtTapLike(e){
+    const db = wx.cloud.database();
+    const  _ = db.command;
+    var that = this;
+    db.collection('comment').doc(this.data.commentId).update({
+      data:{
+        like : _.inc(1)
+      },
+      success:(res)=>{
+        console.log('更新成功');
+        that.setData({
+          commentLike : that.data.commentLike + 1,
+        })
+      },
+      fail:()=>{
+        console.log('更新失败');
+      },
+    })
+  },
+  cmtTapDislike(e){
+    const db = wx.cloud.database();
+    const  _ = db.command;
+    var that = this;
+    db.collection('comment').doc(this.data.commentId).update({
+      data:{
+        dislike : _.inc(1)
+      },
+      success:(res)=>{
+        console.log('更新成功');
+        that.setData({
+          commentDislike : that.data.commentDislike + 1,
+        })
+      },
+      fail:()=>{
+        console.log('更新失败');
+      },
+    })
+  },
+  cfcTapLike(e){
+    const db = wx.cloud.database();
+    const  _ = db.command;
+    var that = this;
+    var index = e.currentTarget.dataset.index;
+    var item = 'cfc.' + index + '.like';
+    var array_item = 'commentCfcList['+index+'].like';
+    db.collection('comment').doc(this.data.commentId).update({
+      data:{
+         [item] : _.inc(1)
+      },
+      success:(res)=>{
+        console.log('更新成功');
+        that.setData({
+          [array_item] : that.data.commentCfcList[index].like + 1,
+        })
+      },
+      fail:()=>{
+        console.log('更新失败');
+      },
+    })
+  },
+  cfcTapDislike(e){
+    const db = wx.cloud.database();
+    const  _ = db.command;
+    var that = this;
+    var index = e.currentTarget.dataset.index;
+    var item = 'cfc.' + index + '.dislike';
+    var array_item = 'commentCfcList['+index+'].dislike';
+    db.collection('comment').doc(this.data.commentId).update({
+      data:{
+         [item] : _.inc(1)
+      },
+      success:(res)=>{
+        console.log('更新成功');
+        that.setData({
+          [array_item] : that.data.commentCfcList[index].dislike + 1,
+        })
+      },
+      fail:()=>{
+        console.log('更新失败');
+      },
     })
   }
 })
