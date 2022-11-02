2aa94c0a7728be264e13b34c85e020b4ace5739e
BruceChen <sharkmansky@163.com>
2022-11-02 21:44:04 +0800

    实现了我的消息功能，现在可以展示自己发的消息以及相关回复，并且可以跳转

--- pages/message/myMessage/myMessage.js
+++ pages/message/myMessage/myMessage.js
@@ -1,106 +1,41 @@
 // pages/group/replyToMe/replyToMe.js
 const app = getApp();
-var timeUtil = require("../../utils/time");
 
 Page({
-
+  data:{
+    comments_array : [],
+  },
   onLoad(options) {
-    this.setData({
-      userName : app.globalData.userInfo.nickName,
+    wx.showLoading({
+      title: '正在加载',
+      mask: true,
     })
+    this.getUserComment();
+  },
 
+  getUserComment() {
     var that = this;
-    const db = wx.cloud.database();
-    db.collection('comment').where({
+    wx.cloud.database().collection('comment').where({
       _openid : app.globalData.userInfo._openid,
     }).get({
-      success:(res)=>{
-        var tempList = [];
-        var dataList = res.data;
-        for(var i = 0; i < dataList.length; ++i){
-          var tempcfc = dataList[i].cfc;
-          var oriCmt = dataList[i].content;
-          var oriCmtId = dataList[i]._id;
-          for(var j = 0; j < tempcfc.length; ++j){
-            var temp = {};
-            temp.replyContent = tempcfc[j].content;
-            temp.replyTime = timeUtil.displayRelativeTime(tempcfc[j].time);
-            temp.replyerName = tempcfc[j].userInfo.nickName;
-            temp.replyerAvatarUrl = tempcfc[j].userInfo.avatarUrl;
-            temp.originComment = (oriCmt.length>10)?(oriCmt.substring(0,10)+'...'):(oriCmt);
-            temp.originCommentId = oriCmtId;
-            tempList.push(temp);
-          }  
-        }
+      success(res) {
+        console.log(res.data);
         that.setData({
-          replyList : tempList,
+          comments_array : res.data,
         })
-        
-      },
-      fail:(res)=>{
-        console.log('查询失败');
-      },
-    })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  //跳转到回复他人的页面
-  reply(e){
-    wx.navigateTo({
-      url: '../replyToMe/replyToOthers/replyToOthers',
-      success:(res)=>{
-        res.eventChannel.emit('acceptDataFromReplyToMe',{
-          commentId : e.currentTarget.dataset.commentid,
-        })
+        wx.hideLoading();
       }
     })
+  },
+  jumpToComment(e) {
+    var comment_id = e.currentTarget.dataset.id;
+    var marker_id = e.currentTarget.dataset.markerid;
+    app.globalData.marker_id = marker_id;
+    app.globalData.comment_id = comment_id;
+    app.globalData.comment_jump = true;
+    wx.switchTab({
+      url: '../../map/map',
+    })
   }
+
 })
